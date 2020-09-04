bca60aabe74adb71cd8f46c146ad76b8e1ba7566
AspectLeft <aspectleft@gmail.com>
2020-09-04 10:14:50 -0700

    Init

---
+++ src/main/java/io/aspectleft/unicodesc/UnicodeScApplication.java
@@ -0,0 +1,13 @@
+package io.aspectleft.unicodesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicodeScApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnicodeScApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/aspectleft/unicodesc/UnicodeScApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aspectleft.unicodesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicodeScApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
